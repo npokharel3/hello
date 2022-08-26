2e366a60f45ec0b673f553662529b48c53124e51
Nancy Pokharel <npokharel3@huskers.unl.edu>
2022-08-26 22:20:02 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Nancy Pokharel
+ *         npokharel3@huskers.unl.edu
+ * Date: 2022/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Nancy Pokharel\n");
+  return 0;
+}
